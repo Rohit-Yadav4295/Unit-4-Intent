7bfc3544edaf1513f2d6e3a70b956609ea845342
Rohit Yadav <rohit33075@gmail.com>
2025-11-09 08:59:14 +0545

    I changed mainactivity

--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    //this is cchanged file
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
